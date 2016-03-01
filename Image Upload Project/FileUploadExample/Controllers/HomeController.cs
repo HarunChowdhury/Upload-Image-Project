31f20bb4e96ff43c7358e5fb9acdfd7792051d15
harunchowdhury <trishamomo1@gmail.com>
2016-03-02 01:28:02 +0600

    Version 01

---
+++ Image Upload Project/FileUploadExample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUploadExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            string path = Server.MapPath("~/Images/" + file.FileName);
+            ViewBag.Path = "File Uploaded Successfully! Wow";
+            file.SaveAs(path);
+            return View();
+        }
+	}
+}
---
+++ Image Upload Project/FileUploadExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileUploadExample.MvcApplication" Language="C#" %>
